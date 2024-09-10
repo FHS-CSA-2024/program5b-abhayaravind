69eeb06badd6e2eacb3bc8593ceb2804bf14ebee
abhayaravind <abhayaravind06@gmail.com>
2024-09-10 15:30:35 -0700

    Update Car.java
    incomplete

--- Car.java
+++ Car.java
@@ -1,1 +1,35 @@
 
+public class Car{
+    private String carName = "";
+    private double mileDriven = 0.0;
+    private double gallonsUsed = 0.0;
+    String x = "x";
+    
+    public Car(){
+        //
+        
+    }
+    
+    public Car(String newName, double newMiles, double gallonsUsed){
+        //
+    }
+    
+    public String getName(){
+        //
+    }
+    // more getters
+    
+    public void setName(String newName){
+        //
+    }
+   // more setters
+    
+    public double calculateAverage(){
+        //read directions
+    }
+    
+    public String toString(){
+        //read direction
+    }
+}
+
